0841eac2051c663a9c90c367697283937629e736
Sydrack Gregório <drasyck1@gmail.com>
2018-08-11 19:23:24 -0300

    11/08/2018 19:23

--- src/providers/config/config.ts
+++ src/providers/config/config.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
 
+let config_key_name = "config";
 /*
   Generated class for the MovieProvider provider.
 
@@ -9,15 +9,51 @@ import { Http } from '@angular/http';
   and Angular DI.
 */
 @Injectable()
-export class MovieProvider {
-  private baseApiPath = "https://api.themoviedb.org/3";
-  constructor(public http: Http) {
-    console.log('Hello MovieProvider Provider');
+export class ConfigProvider {
+
+  private  config = {
+    showSlide: false,
+    name: "",
+    username: ""
   }
 
-  getLatestMovies(){
-    return this.http.get(this.baseApiPath + "/movie/popular?api_key=5d808f9ce7edeb533b47147b59f68e8b");
-  }
+  constructor() {
 
+  }
+  //recuperar os dados
+  getConfigData(): any {
+
+    return localStorage.getItem(config_key_name);
+    
+
+  }
+  //gravar os dados
+  setConfigData(
+    showSlide?: boolean,
+    name?: string,
+    username?: string
+  ) {
+
+    let config = {
+      showSlide: false,
+      name: "",
+      username: ""
+    };
+
+    if (showSlide) {
+      config.showSlide = showSlide;
+    }
+
+    if (name) {
+      config.name = name;
+    }
+
+    if (username) {
+      config.username = username;
+    }
+
+    localStorage.setItem(config_key_name, JSON.stringify(config));
+
+  }
 
 }
